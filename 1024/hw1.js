4fe06ed59575c503996838b55cf0898c57b077d7
CHIAWEN-1024 <411631350@o365.tku.edu.tw>
2024-10-25 01:59:45 +0800

    add

---
+++ 1024/hw1.js
@@ -0,0 +1,5 @@
+var age = 25;
+var Name = "Alice";
+age = 18; //Cannot assign to 'age' because it is a constant.
+Name = "Amy";
+console.log(age, Name);
---
+++ 1024/hw1.ts
@@ -0,0 +1,7 @@
+const age: number = 25
+let Name: string = "Alice"
+
+age = 18; //Cannot assign to 'age' because it is a constant.
+Name = "Amy";
+
+console.log(age,Name);
